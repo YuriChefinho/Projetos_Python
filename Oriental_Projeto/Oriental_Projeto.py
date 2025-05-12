8d352205c14448dcdd5326ee9f0bd2007ef721d8
Yuri Santos <euportinary@gmail.com>
2025-05-12 19:41:14 -0300

    Update Oriental_Projeto.py

--- Oriental_Projeto/Oriental_Projeto.py
+++ Oriental_Projeto/Oriental_Projeto.py
@@ -7,7 +7,7 @@ cardápio = int(input('Selecione o numero de acordo ao cardapio: '))
 
 yakissoba = ['Yakissoba Tradicional', 'Yakissoba Especial', 'Yakissoba Vegetariano'] #Cardapio de Yakissoba
 
-choppSuey = ['Chopp Suey Tradicional', 'Chopp Suey Frango', 'Chopp Suey Camarão']# Cardapio de ChoppSuey
+choppSuey = ['Chopp Suey Tradicional', 'Chopp Suey De Frango', 'Chopp Suey De Camarão']# Cardapio de ChoppSuey
 
 frangoXadrez = ['Frango Xadrez Com Castanhas', 'Frango Xadrez Com Legumes', 'Frango Xadrez Com Champignon'] 
 
@@ -58,20 +58,50 @@ if cardápio == 1:
 
 elif cardápio == 2:
     print('CARDAPIO DE CHOPP SUEY: ')
-    print('Chopp Suey Tracidional [1] \nChopp Suey Frango      [2] \nChopp Suey Camarão     [3]')
+    print('Chopp Suey Tracidional [1] \nChopp Suey De Frango      [2] \nChopp Suey De Camarão     [3]')
     cardápio_choppSuey = int(input('Digite o numero do prato da sua escolha: '))    #Variavel Cardapio Chopp Suey 
+
     if cardápio_choppSuey == 1:
-        print(choppSuey[0])
+        valor_choppSuey_tradicional = 45 #Valor do ChoppSuey Tradicional R$45,00
+        print(f'{choppSuey[0]} = {valor_choppSuey_tradicional:.2f} \nConfirmar Compra? ')
+        compra = int(input('SIM [ 1 ] \nNÃO [ 2 ] : '))
+        if compra == 1:
+            print('!!!OBRIGADO PELA COMPRA!!!')
+            print(f' 1 - Chopp Suey Tradicional | R${valor_choppSuey_tradicional:.2f} \n\n\nVALOR TOTAL = R${valor_choppSuey_tradicional:.2f}')
+        else:
+            print('VOLTE SEMPRE! ')
+                  
 
     elif cardápio_choppSuey == 2:
-        print(choppSuey[1])
+        valor_choppSuey_frango = 40 #Valor do ChoppSuey de Frango R$40,00
+        print(f'{choppSuey[1]} = {valor_choppSuey_frango:.2f} \nConfirmar Compra? ')
+        compra = int(input('SIM [ 1 ] \nNÃO [ 2 ] : '))
+        if compra == 1:
+            print('!!!OBRIGADO PELA COMPRA!!!')
+            print(f' 1 - Chopp Suey De Frango | R${valor_choppSuey_frango:.2f} \n\n\nVALOR TOTAL = R${valor_choppSuey_frango:.2f}')
+        else:
+            print('VOLTE SEMPRE! ')
+
 
     elif cardápio_choppSuey == 3:
-        print(choppSuey[2])
+        valor_choppSuey_camarão = 55 #Valor do ChoppSuey de Camarão R$55,00
+        print(f'{choppSuey[2]} = {valor_choppSuey_camarão:.2f} \nConfirmar Compra? ')
+        compra = int(input('SIM [ 1 ] \nNÃO [ 2 ] : '))
+        if compra == 1:
+            print('!!!OBRIGADO PELA COMPRA!!!')
+            print(f' 1 - Chopp Suey De Camarão | R${valor_choppSuey_camarão:.2f} \n\n\nVALOR TOTAL = R${valor_choppSuey_camarão:.2f}')
+        else:
+            print('VOLTE SEMPRE! ')
+
+
+
+
+
 
 
 
 #Estrutura DO Cardapio De Frango Xadrez
+
 elif cardápio == 3:
     print('CARDAPIO DE FRANGO XADREZ: ')
     print('Frango Xadrez Com Castanhas  [1] \nFrango Xadrez Com Legumes    [2] \nFrango Xadrez Com Champignon [3]')
